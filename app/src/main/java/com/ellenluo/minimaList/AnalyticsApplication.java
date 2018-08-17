972e5ca521cad6dde762a818b354a6daade3e072
ellenluo <ellen.luo@berkeley.edu>
2018-08-17 16:12:48 -0400

    Update Android version and fix Google Analytics

--- app/src/main/java/com/ellenluo/minimaList/AnalyticsApplication.java
+++ app/src/main/java/com/ellenluo/minimaList/AnalyticsApplication.java
@@ -1,25 +1,52 @@
-package com.ellenluo.minimaList;
-
-/**
- * AnalyticsApplication
- * Created by Ellen Luo and Google Analytics
- * Application that allows Google Analytics tracking
+/*
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
+package com.ellenluo.minimaList;
 
 import android.app.Application;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
 
+/**
+ * This is a subclass of {@link Application} used to provide shared objects for this app, such as
+ * the {@link Tracker}.
+ */
 public class AnalyticsApplication extends Application {
-    private Tracker mTracker;
 
+    private static GoogleAnalytics sAnalytics;
+    private static Tracker sTracker;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        sAnalytics = GoogleAnalytics.getInstance(this);
+    }
+
+    /**
+     * Gets the default {@link Tracker} for this {@link Application}.
+     * @return tracker
+     */
     synchronized public Tracker getDefaultTracker() {
-        if (mTracker == null) {
-            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-            mTracker = analytics.newTracker(R.xml.analytics);
-            analytics.enableAutoActivityReports(this);
+        // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+        if (sTracker == null) {
+            sTracker = sAnalytics.newTracker(R.xml.global_tracker);
         }
-        return mTracker;
+
+        return sTracker;
     }
 }
